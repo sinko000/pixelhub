bf9b02243c3d31803ccf1421595a8c9b7e5f7b2b
sinko000 <samiroglu70@gmail.com>
2026-07-22 17:58:48 +0400

    Improve server startup log message

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Frontend fayllarını (HTML, CSS, JS) oxuması üçün
+app.use(express.static(path.join(__dirname, 'public'))); // və ya sadəcə ana qovluq
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server ${PORT} portunda işləyir`);
+    console.log(`Server ${PORT} portunda uğurla işləyir!`);
 });
