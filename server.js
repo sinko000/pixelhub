220cc830fd86ad7a6421eb681a07fd0b8e2f4f03
sinko000 <samiroglu70@gmail.com>
2026-07-22 18:00:20 +0400

    Modify static file serving and add index route
    Updated static file serving to read from the root directory and added a route to serve index.html.

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Frontend fayllarını (HTML, CSS, JS) oxuması üçün
-app.use(express.static(path.join(__dirname, 'public'))); // və ya sadəcə ana qovluq
+// Ana qovluqdakı HTML, CSS və JS fayllarını birbaşa oxuması üçün:
+app.use(express.static(__dirname));
+
+// Əgər saytın kök ünvanına (/) girilərsə, index.html faylını göstər:
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
